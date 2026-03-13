e2a8d403911fb00703c3ef20f214a86972aec935
Josh Zak <joshuazak19@gmail.com>
2026-03-13 19:26:29 -0400

    Started templating

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,62 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="main">
+    <div id="header">
+        <div class="name">Josh Zak - FullStack Engineer</div>
+        <div class="nav">
+            <div class="about">About</div>
+            <div class="projects">Projects</div>
+        </div>
+    </div>
+    <div class="section1">
+
+    </div>
+    <div class="section2">
+
+    </div>
+    <div class="section3">
+
+    </div>
+    <div class="footer">
+
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+
+
+
+
+
+
+<style>
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+}
+
+
+.main{
+    display: flex;
+    flex-direction: column;
+}
+
+.main > *{
+    border: solid black;
+}
+
+#header{
+    display: flex;
+    justify-content: space-between;
+    padding: 0 1rem;
+}
+
+.nav{
+    display: flex;
+    gap: 2rem;
+}
+
+</style>
